6f431ab960bc90a6180c4d3150645db5eac0f95a
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-03-23 13:01:32 +0000

    Abstracted away the mechanism to find the base roman numeral value

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -2,21 +2,39 @@ object RomanNumerals {
 
   private val fixedDigits = Map(
     4 -> "IV",
-    9 -> "IX"
+    9 -> "IX",
+    14 -> "XIV"
   )
 
-  def convert(digit: Int): String =
+  private val base = Seq(
+    5 -> "V",
+    10 -> "X",
+    15 -> "XV"
+  )
+
+  def convert(digit: Int): String = {
+    val base = findBase(digit)
+
     if (fixedDigits.contains(digit))
       fixedDigits(digit)
     else {
       if (digit < 4)
         "I" * digit
       else if (digit < 9)
-        "V" + "I" * (digit - 5)
+        base + "I" * (digit - 5)
       else if (digit < 14)
-        "X" + "I" * (digit - 10)
+        base + "I" * (digit - 10)
       else
-        "XIV"
+        base
     }
+  }
+
+  private def findBase(digit: Int): String = {
+     base.find(k => digit <= k._1) match {
+       case Some((key, value)) if key == digit => value
+       case x@Some(_) if base.indexOf(x.get) > 0 => base(base.indexOf(x.get) - 1)._2
+       case _ => ""
+     }
+  }
 
 }
