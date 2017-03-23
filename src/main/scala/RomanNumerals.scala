d6854d48948c1d4aabf5a998bbe75cdff4687e57
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-03-23 12:38:45 +0000

    Refactored known exceptions to a map

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,15 +1,20 @@
 object RomanNumerals {
 
+  private val fixedDigits = Map(
+    4 -> "IV",
+    9 -> "IX"
+  )
+
   def convert(digit: Int): String =
-    if (digit < 4)
-      "I" * digit
-    else if (digit == 4)
-      "IV"
-    else if (digit < 9)
-      "V" + "I" * (digit - 5)
-    else if (digit == 9)
-      "IX"
-    else
-      "X"
+    if (fixedDigits.contains(digit))
+      fixedDigits(digit)
+    else {
+      if (digit < 4)
+        "I" * digit
+      else if (digit < 9)
+        "V" + "I" * (digit - 5)
+      else
+        "X"
+    }
 
 }
