3beac0987efffa8e96b7b993dc35db8de2a6be6a
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-03-23 12:28:54 +0000

    Refactored convert to enumerate digits instead of if/else

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,11 +1,5 @@
 object RomanNumerals {
 
-  def convert(digit: Int): String =
-    if (digit == 1)
-      "I"
-    else if (digit == 2)
-      "II"
-    else
-      "III"
+  def convert(digit: Int): String = "I" * digit
 
 }
